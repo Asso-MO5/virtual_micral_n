5c4920edc8d30a0b560e6cae12c26941c99417db
Sylvain Glaize <mokona@puupuu.org>
2021-01-31 00:47:31 +0100

    Add specific time value for un-scheduling a device.

--- src/emulation_core/src/Scheduling.h
+++ src/emulation_core/src/Scheduling.h
@@ -2,10 +2,12 @@
 #define MICRALN_SCHEDULING_H
 
 #include <cstdint>
+#include <limits>
 
 namespace Scheduling
 {
     using counter_type = uint64_t;
+    inline counter_type unscheduled() { return std::numeric_limits<counter_type>::max(); }
 }
 
 #endif //MICRALN_SCHEDULING_H
