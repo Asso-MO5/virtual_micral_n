dc7e9cff1c174585a8c4754417c2a8aa1e8021cd
Sylvain Glaize <mokona@puupuu.org>
2021-01-31 00:57:11 +0100

    Fix Clock name in its tests.

--- src/devices/tests/clock_test.cpp
+++ src/devices/tests/clock_test.cpp
@@ -4,33 +4,33 @@
 
 using namespace testing;
 
-TEST(Devices, clock_is_a_schedulable)
+TEST(Clock, clock_is_a_schedulable)
 {
     Clock clock{400'000_hz};
     ASSERT_THAT(clock.get_next_activation_time(), Eq(0));
 }
 
-TEST(Devices, clock_steps_at_the_correct_frequency)
+TEST(Clock, clock_steps_at_the_correct_frequency)
 {
     Clock clock{400'000_hz};
     clock.step();
     ASSERT_THAT(clock.get_next_activation_time(), Eq(2500 / 2));
 }
 
-TEST(Devices, clock_starts_low)
+TEST(Clock, clock_starts_low)
 {
     Clock clock{400'000_hz};
     ASSERT_THAT(clock.get_state(), Eq(State::LOW));
 }
 
-TEST(Devices, clock_changes_state)
+TEST(Clock, clock_changes_state)
 {
     Clock clock{400'000_hz};
     clock.step();
     ASSERT_THAT(clock.get_state(), Eq(State::HIGH));
 }
 
-TEST(Devices, clock_triggers_on_edge)
+TEST(Clock, clock_triggers_on_edge)
 {
     Clock clock{400'000_hz};
 
