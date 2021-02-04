5c0adc101c0403ec2b0234a863a443d9e361476c
Sylvain Glaize <mokona@puupuu.org>
2021-02-04 23:57:47 +0100

    8008 PC advances and is emitted on BUS.

--- src/devices/src/CPU8008.cpp
+++ src/devices/src/CPU8008.cpp
@@ -1,20 +1,24 @@
 #include "CPU8008.h"
 #include <emulation_core/src/Edge.h>
 
+#include <iostream>
+
 namespace
 {
     const Scheduling::counter_type BOOT_UP_TIME = 16'000;
 
     namespace Timings
     {
-        const Scheduling::counter_type MIN_CLOCK_PERIOD = 2000;
-        const Scheduling::counter_type MAX_CLOCK_PERIOD = 3000;
-        const Scheduling::counter_type PULSE_WIDTH_PHASE_1 = 700;
-        const Scheduling::counter_type PULSE_WIDTH_PHASE_2 = 550;
-        const Scheduling::counter_type FALLING_1_TO_FALLING_2_EDGE_MIN = 900;
-        const Scheduling::counter_type FALLING_1_TO_FALLING_2_EDGE_MAX = 1100;
-        const Scheduling::counter_type DELAY_FROM_PHASE_1_TO_2 = 200;
-        const Scheduling::counter_type DELAY_FROM_PHASE_2_TO_1 = 400;
+        using namespace Scheduling;
+        const counter_type MIN_CLOCK_PERIOD = 2000;
+        const counter_type MAX_CLOCK_PERIOD = 3000;
+        const counter_type PULSE_WIDTH_PHASE_1 = 700;
+        const counter_type PULSE_WIDTH_PHASE_2 = 550;
+        const counter_type FALLING_1_TO_FALLING_2_EDGE_MIN = 900;
+        const counter_type FALLING_1_TO_FALLING_2_EDGE_MAX = 1100;
+        const counter_type DELAY_FROM_PHASE_1_TO_2 = 200;
+        const counter_type DELAY_FROM_PHASE_2_TO_1 = 400;
+        const counter_type DATA_OUT_HOLD_TIME = 100;
     } // namespace Timings
 
 } // namespace
@@ -24,10 +28,8 @@ bool operator<(const CPU8008::NextEventType& a, const CPU8008::NextEventType& b)
     return std::get<0>(a) > std::get<0>(b);
 }
 
-CPU8008::CPU8008(SignalReceiver& scheduler)
-    : scheduler(scheduler), next_state(CpuState::STOPPED), is_first_phase_cycle(true)
+CPU8008::CPU8008(SignalReceiver& scheduler) : scheduler(scheduler)
 {
-    output_pins.state = CpuState::STOPPED;
     output_pins.sync = ::State::LOW;
 }
 
@@ -59,11 +61,45 @@ void CPU8008::step()
             // TODO: Constraint : tS1 max (1.1) after ø11 RAISE (for others)
             output_pins.state = static_cast<CpuState>(param);
 
+            switch (output_pins.state)
+            {
+                case CpuState::T1I:
+                    io_data_latch = address_stack.get_low_pc_no_inc();
+                    break;
+                case CpuState::T1:
+                    io_data_latch = address_stack.get_low_pc_and_inc();
+                    break;
+                case CpuState::WAIT:
+                    break;
+                case CpuState::T3:
+                    break;
+                case CpuState::STOPPED:
+                    break;
+                case CpuState::T2:
+                    io_data_latch = address_stack.get_high_pc();
+                    break;
+                case CpuState::T5:
+                    break;
+                case CpuState::T4:
+                    break;
+            }
+
             break;
         }
         case DATA_OUT:
+            if (param)
+            {
+                assert(data_pins.taken == false);
+                data_pins.data = io_data_latch;
+                data_pins.taken = true;
+            }
+            else
+            {
+                assert(data_pins.taken == true);
+                data_pins.taken = false;
+            }
             break;
-        case DATA_INT:
+        case DATA_IN:
             break;
     }
 
@@ -79,14 +115,100 @@ void CPU8008::step()
     /*
      * Events
      * DATA_OUT (DATA) : Change by ø11
-        *  Constraint : tDD max (1.0) after ø11 RAISE
-        *  Constraint : tOH min (.10) after
+        *  Constraint : tDD max (1.0) after ø11 FALLING
+        *  Constraint : tOH min (.10) after ø22 FALLING
      * DATA_IN // SAMPLING : After DATA OUT
      */
 }
 
 const CPU8008::OutputPins& CPU8008::get_output_pins() const { return output_pins; }
 const CPU8008::DataPins& CPU8008::get_data_pins() const { return data_pins; }
+
+void CPU8008::on_signal_11_raising(Scheduling::counter_type edge_time)
+{
+    next_events.push(std::make_tuple(edge_time + 20, SYNC, 1));
+
+    switch (output_pins.state)
+    {
+        case CpuState::STOPPED:
+            if (interrupt_pending)
+            {
+                interrupt_pending = false;
+
+                next_events.push(
+                        std::make_tuple(edge_time + 25, STATE, static_cast<int>(CpuState::T1I)));
+            }
+            break;
+        case CpuState::WAIT:
+            break;
+        case CpuState::T1:
+        case CpuState::T1I:
+            next_events.push(
+                    std::make_tuple(edge_time + 25, STATE, static_cast<int>(CpuState::T2)));
+            break;
+        case CpuState::T2:
+            next_events.push(
+                    std::make_tuple(edge_time + 25, STATE, static_cast<int>(CpuState::T3)));
+            break;
+        case CpuState::T3:
+            next_events.push(
+                    std::make_tuple(edge_time + 25, STATE, static_cast<int>(CpuState::T1)));
+            break;
+        case CpuState::T4:
+            next_events.push(
+                    std::make_tuple(edge_time + 25, STATE, static_cast<int>(CpuState::T5)));
+            break;
+        case CpuState::T5:
+            next_events.push(
+                    std::make_tuple(edge_time + 25, STATE, static_cast<int>(CpuState::T1)));
+
+            break;
+    }
+}
+
+void CPU8008::on_signal_12_raising(Scheduling::counter_type edge_time)
+{
+    next_events.push(std::make_tuple(edge_time + 20, SYNC, 0));
+}
+
+void CPU8008::on_signal_21_raising(Scheduling::counter_type edge_time)
+{
+    switch (output_pins.state)
+    {
+        case CpuState::WAIT:
+        case CpuState::STOPPED:
+            break;
+        case CpuState::T1I:
+        case CpuState::T1:
+        case CpuState::T2:
+            next_events.push(std::make_tuple(edge_time + 20, DATA_OUT, 1));
+            break;
+        case CpuState::T3:
+        case CpuState::T4:
+        case CpuState::T5:
+            break;
+    }
+}
+
+void CPU8008::on_signal_22_falling(Scheduling::counter_type edge_time)
+{
+    switch (output_pins.state)
+    {
+        case CpuState::WAIT:
+        case CpuState::STOPPED:
+            break;
+        case CpuState::T1I:
+        case CpuState::T1:
+        case CpuState::T2:
+            next_events.push(
+                    std::make_tuple(edge_time + Timings::DATA_OUT_HOLD_TIME + 20, DATA_OUT, 0));
+            break;
+        case CpuState::T3:
+        case CpuState::T4:
+        case CpuState::T5:
+            break;
+    }
+}
 
 void CPU8008::signal_phase_1(Edge edge)
 {
@@ -102,72 +224,51 @@ void CPU8008::signal_phase_1(Edge edge)
     {
         if (is_first_phase_cycle)
         {
-            switch (output_pins.state)
-            {
-                case CpuState::STOPPED:
-                case CpuState::WAIT:
-                    output_pins.state = next_state; // Shortcut
-                    break;
-                case CpuState::T1:
-                case CpuState::T1I:
-                case CpuState::T2:
-                case CpuState::T3:
-                case CpuState::T4:
-                case CpuState::T5:
-                    next_events.push(std::make_tuple(edge_time + 20, SYNC, 1));
-                    break;
-            }
-            switch (output_pins.state)
-            {
-                case CpuState::STOPPED:
-                case CpuState::WAIT:
-                    output_pins.state = next_state; // Shortcut
-                    break;
-                case CpuState::T1:
-                case CpuState::T1I:
-                    next_events.push(
-                            std::make_tuple(edge_time + 25, STATE, static_cast<int>(CpuState::T2)));
-                    break;
-                case CpuState::T2:
-                    next_events.push(
-                            std::make_tuple(edge_time + 25, STATE, static_cast<int>(CpuState::T3)));
-                    break;
-                case CpuState::T3:
-                    next_events.push(
-                            std::make_tuple(edge_time + 25, STATE, static_cast<int>(CpuState::T4)));
-                    break;
-                case CpuState::T4:
-                    next_events.push(
-                            std::make_tuple(edge_time + 25, STATE, static_cast<int>(CpuState::T5)));
-                    break;
-                case CpuState::T5:
-                    next_events.push(
-                            std::make_tuple(edge_time + 25, STATE, static_cast<int>(CpuState::T1)));
-
-                    break;
-            }
+            on_signal_11_raising(edge_time);
         }
         else
         {
-            switch (output_pins.state)
-            {
-                case CpuState::STOPPED:
-                case CpuState::WAIT:
-                    output_pins.state = next_state; // Shortcut
-                    break;
-                case CpuState::T1:
-                case CpuState::T1I:
-                case CpuState::T2:
-                case CpuState::T3:
-                case CpuState::T4:
-                case CpuState::T5:
-                    next_events.push(std::make_tuple(edge_time + 20, SYNC, 0));
-                    break;
-            }
+            on_signal_12_raising(edge_time);
+        }
+    }
+    else
+    {
+    }
+
+    schedule_next_event(edge_time);
+}
+
+void CPU8008::signal_phase_2(Edge edge)
+{
+    if (input_pins.vdd == ::State::LOW)
+    {
+        set_next_activation_time(Scheduling::unscheduled());
+        return;
+    }
+
+    auto edge_time = edge.time();
+
+    if (edge == Edge::Front::RISING)
+    {
+        if (is_first_phase_cycle)
+        {
+            on_signal_21_raising(edge_time);
+        }
+    }
+    else
+    {
+        if (!is_first_phase_cycle)
+        {
+            on_signal_22_falling(edge_time);
         }
         is_first_phase_cycle = !is_first_phase_cycle;
     }
 
+    schedule_next_event(edge_time);
+}
+
+void CPU8008::schedule_next_event(Scheduling::counter_type edge_time)
+{
     if (next_events.empty())
     {
         set_next_activation_time(edge_time);
@@ -179,8 +280,6 @@ void CPU8008::signal_phase_1(Edge edge)
     }
     scheduler.change_schedule(get_id());
 }
-
-void CPU8008::signal_phase_2(Edge edge) {}
 
 void CPU8008::signal_vdd(Edge edge)
 {
@@ -196,15 +295,12 @@ void CPU8008::signal_interrupt(Edge edge)
     if (input_pins.interrupt == ::State::HIGH)
     {
         // TODO: acknowledge the interruption with correct timing
-        // (by timestamping the state change for example)
 
         if ((edge.time() - input_pins.vdd.last_change()) < BOOT_UP_TIME)
         {
             // TODO: set garbage in the CPU state. It's too early
         }
-
-        // TODO: acknowledge of interrupt should be immediate.
-        next_state = CpuState::T1I;
+        interrupt_pending = true;
     }
 }
 void CPU8008::register_sync_trigger(std::function<void(Edge)> callback)
--- src/devices/src/CPU8008.h
+++ src/devices/src/CPU8008.h
@@ -14,20 +14,29 @@ class CPU8008 : public SchedulableImpl
 public:
     enum class CpuState : uint8_t
     {
-        WAIT = 0,
-        T3 = 1,
-        T1 = 2,
-        STOPPED = 3,
-        T2 = 4,
-        T5 = 5,
-        T1I = 6,
-        T4 = 7,
+        WAIT = 0b000,
+        T3 = 0b001,
+        T1 = 0b010,
+        STOPPED = 0b011,
+        T2 = 0b100,
+        T5 = 0b101,
+        T1I = 0b110,
+        T4 = 0b111,
+    };
+
+    // All values are shifted to high bits so they can be easily OR'd with address on T2.
+    enum class CycleControl : uint8_t
+    {
+        PCI = 0b00000000, // Memory read for the first byte of instruction
+        PCR = 0b01000000, // Memory read for data or additional bytes of instruction
+        PCC = 0b10000000, // Data is a command for I/O operation
+        PCW = 0b11000000, // Memory write for data
     };
 
     struct OutputPins
     {
-        CpuState state; // Would it be interesting to pack to 3 bits
-        ::State sync;   // 1 bit
+        CpuState state{CpuState::STOPPED};
+        State sync{State::LOW};
     };
 
     struct InputPins
@@ -42,9 +51,10 @@ public:
     struct DataPins
     {
         uint8_t data; // Maybe no need ? (or to force typing ?)
+        bool taken{}; // True if the CPU is applying output
     };
 
-    CPU8008(SignalReceiver & scheduler);
+    explicit CPU8008(SignalReceiver & scheduler);
 
     void step() override;
     [[nodiscard]] const OutputPins& get_output_pins() const;
@@ -62,23 +72,32 @@ public:
         SYNC,
         STATE,
         DATA_OUT,
-        DATA_INT
+        DATA_IN
     };
     using NextEventType = std::tuple<Scheduling::counter_type, Event, uint8_t>;
 
 private:
-    SignalReceiver & scheduler;
     OutputPins output_pins{};
     DataPins data_pins{};
     InputPins input_pins{};
-
     AddressStack address_stack;
-    CpuState next_state;
-    bool is_first_phase_cycle; // Complete cycle
+    uint8_t io_data_latch{};
 
+    CycleControl cycle_control{CycleControl::PCI};
+    bool is_first_phase_cycle{true};
+    bool interrupt_pending{};
+
+    SignalReceiver & scheduler;
     std::priority_queue<NextEventType, std::vector<NextEventType>> next_events;
-
     std::function<void(Edge)> sync_callback = [](Edge) {};
+
+    void on_signal_11_raising(Scheduling::counter_type edge_time);
+    void on_signal_12_raising(Scheduling::counter_type edge_time);
+    void on_signal_21_raising(Scheduling::counter_type edge_time);
+    void on_signal_22_falling(Scheduling::counter_type edge_time);
+
+    void schedule_next_event(Scheduling::counter_type edge_time);
+
 };
 
 #endif //MICRALN_CPU8008_H
