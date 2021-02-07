6d8df5bdd5d6ecd8e5c3041773287a2c696f0a6c
Sylvain Glaize <mokona@puupuu.org>
2021-02-07 20:56:04 +0100

    Extract connections between ROM into a ControlBus

--- src/cli/src/main.cpp
+++ src/cli/src/main.cpp
@@ -4,8 +4,69 @@
 #include <devices/src/DoubleClock.h>
 #include <devices/src/SimpleROM.h>
 #include <loguru.hpp>
+#include <utility>
 
 const char* STATE_STRINGS[] = {"WAIT", "T3", "T1", "STOPPED", "T2", "T5", "T1I", "T4"};
+
+class ControlBus
+{
+public:
+    ControlBus(std::shared_ptr<CPU8008> cpu, std::shared_ptr<SimpleROM> rom)
+        : cpu(std::move(cpu)), rom(std::move(rom))
+    {}
+
+    void signal_phase_1(const Edge& edge)
+    {
+        if (edge == Edge::Front::RISING)
+        {
+            if (cpu->get_output_pins().sync == State::HIGH &&
+                cpu->get_output_pins().state == CPU8008::CpuState::T3)
+            {
+                assert(cpu->get_data_pins().taken == false);
+                rom->signal_output_enable(Edge{Edge::Front::FALLING, edge.time()});
+                rom->signal_chip_select(Edge{Edge::Front::FALLING, edge.time()});
+            }
+        }
+        else
+        {
+            // ROM enabled
+            if (cpu->get_output_pins().sync == State::HIGH &&
+                cpu->get_output_pins().state == CPU8008::CpuState::T3)
+            {
+                assert(cpu->get_data_pins().taken == false);
+                rom->signal_chip_select(Edge{Edge::Front::RISING, edge.time()});
+                rom->signal_output_enable(Edge{Edge::Front::RISING, edge.time()});
+            }
+        }
+    }
+
+    void signal_phase_2(const Edge& edge)
+    {
+        if (edge == Edge::Front::RISING)
+        {
+            // Read data address from the CPU
+            if (cpu->get_output_pins().sync == State::LOW &&
+                cpu->get_output_pins().state == CPU8008::CpuState::T1)
+            {
+                rom_address_bus |= cpu->get_data_pins().data;
+            }
+            if (cpu->get_output_pins().sync == State::LOW &&
+                cpu->get_output_pins().state == CPU8008::CpuState::T2)
+            {
+                rom_address_bus |= (cpu->get_data_pins().data & 0x3f) << 8;
+                rom->set_address(rom_address_bus);
+            }
+        }
+        else
+        {
+        }
+    }
+
+private:
+    std::shared_ptr<CPU8008> cpu;
+    std::shared_ptr<SimpleROM> rom;
+    uint16_t rom_address_bus{};
+};
 
 int main(int argc, char** argv)
 {
@@ -33,54 +94,23 @@ int main(int argc, char** argv)
     LOG_F(INFO, "Creates the Clock");
     auto clock = std::make_shared<DoubleClock>(500'000_hz);
 
+    ControlBus control_bus(cpu, rom);
+
     int counting = 0;
-    clock->register_phase_1_trigger([&counting, &cpu, &rom](Edge edge) {
-        cpu->signal_phase_1(edge);
+    clock->register_phase_1_trigger([&counting, &cpu, &control_bus](Edge edge) {
         if (edge == Edge::Front::RISING)
         {
             counting += 1;
-            if (cpu->get_output_pins().sync == State::HIGH &&
-                cpu->get_output_pins().state == CPU8008::CpuState::T3)
-            {
-                assert(cpu->get_data_pins().taken == false);
-                rom->signal_output_enable(Edge{Edge::Front::FALLING, edge.time()});
-                rom->signal_chip_select(Edge{Edge::Front::FALLING, edge.time()});
-            }
         }
-        else
-        {
-            // ROM enabled
-            if (cpu->get_output_pins().sync == State::HIGH &&
-                cpu->get_output_pins().state == CPU8008::CpuState::T3)
-            {
-                assert(cpu->get_data_pins().taken == false);
-                rom->signal_chip_select(Edge{Edge::Front::RISING, edge.time()});
-                rom->signal_output_enable(Edge{Edge::Front::RISING, edge.time()});
-            }
-        }
+        cpu->signal_phase_1(edge);
+        control_bus.signal_phase_1(edge);
     });
 
-    static uint16_t rom_address_bus;
-    clock->register_phase_2_trigger([&cpu, &rom](Edge edge) {
+    //connect(&DoubleClock::register_phase_1_trigger, &CPU8008::signal_phase_1);
+
+    clock->register_phase_2_trigger([&cpu, &control_bus](Edge edge) {
         cpu->signal_phase_2(edge);
-        if (edge == Edge::Front::RISING)
-        {
-            // Read data address from the CPU
-            if (cpu->get_output_pins().sync == State::LOW &&
-                cpu->get_output_pins().state == CPU8008::CpuState::T1)
-            {
-                rom_address_bus |= cpu->get_data_pins().data;
-            }
-            if (cpu->get_output_pins().sync == State::LOW &&
-                cpu->get_output_pins().state == CPU8008::CpuState::T2)
-            {
-                rom_address_bus |= (cpu->get_data_pins().data & 0x3f) << 8;
-                rom->set_address(rom_address_bus);
-            }
-        }
-        else
-        {
-        }
+        control_bus.signal_phase_2(edge);
     });
 
     LOG_F(INFO, "Adds devices to the scheduler");
