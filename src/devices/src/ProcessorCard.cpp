082342cbc5578d869a7c540040d1f506e0fa4bfb
Sylvain Glaize <mokona@puupuu.org>
2021-02-26 23:26:26 +0100

    Add a ProcessorCard.

---
+++ src/devices/src/ProcessorCard.cpp
@@ -0,0 +1,22 @@
+#include "ProcessorCard.h"
+
+#include <utility>
+
+#include <devices/src/CPU8008.h>
+#include <devices/src/InterruptAtStart.h>
+#include <devices/src/InterruptController.h>
+#include <devices/src/Pluribus.h>
+
+ProcessorCard::ProcessorCard(ProcessorCard::Config config)
+    : pluribus{std::move(config.pluribus)}, cpu{std::move(config.cpu)},
+      interrupt_controller{std::move(config.interrupt_controller)},
+      interrupt_at_start{std::move(config.interrupt_at_start)}
+{
+    set_next_activation_time(Scheduling::unscheduled());
+}
+
+const CPU8008& ProcessorCard::get_cpu() const { return *cpu; }
+InterruptController& ProcessorCard::get_interrupt_controller() { return *interrupt_controller; }
+void ProcessorCard::set_wait_line(Edge edge) { cpu->signal_wait(edge); }
+
+void ProcessorCard::step() {}
---
+++ src/devices/src/ProcessorCard.h
@@ -0,0 +1,42 @@
+#ifndef MICRALN_PROCESSORCARD_H
+#define MICRALN_PROCESSORCARD_H
+
+#include <emulation_core/src/Edge.h>
+#include <emulation_core/src/Schedulable.h>
+#include <memory>
+
+class Pluribus;
+class CPU8008;
+class InterruptController;
+class InterruptAtStart;
+class DoubleClock;
+
+class ProcessorCard : public SchedulableImpl
+{
+public:
+    struct Config
+    {
+        std::shared_ptr<Pluribus> pluribus;
+        std::shared_ptr<DoubleClock> clock;
+        std::shared_ptr<CPU8008> cpu;
+        std::shared_ptr<InterruptController> interrupt_controller;
+        std::shared_ptr<InterruptAtStart> interrupt_at_start;
+    };
+
+    explicit ProcessorCard(Config config);
+    ~ProcessorCard() override = default;
+
+    void step() override;
+
+    [[nodiscard]] const CPU8008& get_cpu() const;
+    [[nodiscard]] InterruptController& get_interrupt_controller();
+    void set_wait_line(Edge edge);
+
+private:
+    std::shared_ptr<Pluribus> pluribus;
+    std::shared_ptr<CPU8008> cpu{};
+    std::shared_ptr<InterruptController> interrupt_controller;
+    std::shared_ptr<InterruptAtStart> interrupt_at_start;
+};
+
+#endif //MICRALN_PROCESSORCARD_H
