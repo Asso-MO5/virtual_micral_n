4b336b4bcc862cac332fcc06b2ecf7c2c227cb42
Sylvain Glaize <mokona@puupuu.org>
2021-02-27 20:53:24 +0100

    Add output data lines on the Pluribus.

--- src/devices/src/Pluribus.cpp
+++ src/devices/src/Pluribus.cpp
@@ -1,1 +1,6 @@
 #include "Pluribus.h"
+
+#include <emulation_core/src/DataBus.h>
+
+Pluribus::Pluribus() { data_bus_d0_7 = std::make_shared<DataBus>();
+}
--- src/devices/src/Pluribus.h
+++ src/devices/src/Pluribus.h
@@ -1,8 +1,28 @@
 #ifndef MICRALN_PLURIBUS_H
 #define MICRALN_PLURIBUS_H
 
+#include <emulation_core/src/OwnedSignal.h>
+#include <memory>
+
+class DataBus;
+
 class Pluribus
 {
+public:
+    Pluribus();
+
+    OwnedSignal vdd;
+    OwnedSignal phase_2;
+    OwnedSignal sync;
+    OwnedSignal t2;
+    OwnedSignal t3;
+    OwnedSignal t3prime;
+    OwnedSignal cc0;
+    OwnedSignal cc1;
+    OwnedSignal wait;
+    OwnedSignal stop;
+
+    std::shared_ptr<DataBus> data_bus_d0_7{};
 };
 
 #endif //MICRALN_PLURIBUS_H
