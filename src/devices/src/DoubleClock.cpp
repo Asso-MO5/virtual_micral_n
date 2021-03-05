6f7e644633361d2cfc88449101267798996be35b
Sylvain Glaize <mokona@puupuu.org>
2021-03-06 00:03:48 +0100

    DoubleClock is based on OwnedSignal.

--- src/devices/src/DoubleClock.cpp
+++ src/devices/src/DoubleClock.cpp
@@ -1,7 +1,5 @@
 #include "DoubleClock.h"
 
-#include <utility>
-
 namespace Timings
 {
     const Scheduling::counter_type PULSE_WIDTH_PHASE_1 = 700;
@@ -9,7 +7,10 @@ namespace Timings
     const Scheduling::counter_type DELAY_FROM_PHASE_1_TO_2 = 200;
 } // namespace Timings
 
-DoubleClock::DoubleClock(Frequency frequency) : main_frequency(frequency) {}
+DoubleClock::DoubleClock(Frequency frequency) : main_frequency(frequency) {
+    phase_1.request(this);
+    phase_2.request(this);
+}
 
 void DoubleClock::step()
 {
@@ -17,24 +18,20 @@ void DoubleClock::step()
     switch (phase_count)
     {
         case 0:
-            phase_1 = State::HIGH;
-            edge_1_callback(Edge{Edge::Front::RISING, next_activation_time});
+            phase_1.set(State::HIGH, next_activation_time, this);
             next_phase_1 = next_activation_time + main_frequency.get_period_as_ns();
             next_activation_time += Timings::PULSE_WIDTH_PHASE_1;
             break;
         case 1:
-            phase_1 = State::LOW;
-            edge_1_callback(Edge{Edge::Front::FALLING, next_activation_time});
+            phase_1.set(State::LOW, next_activation_time, this);
             next_activation_time += Timings::DELAY_FROM_PHASE_1_TO_2;
             break;
         case 2:
-            phase_2 = State::HIGH;
-            edge_2_callback(Edge{Edge::Front::RISING, next_activation_time});
+            phase_2.set(State::HIGH, next_activation_time, this);
             next_activation_time += Timings::PULSE_WIDTH_PHASE_2;
             break;
         case 3:
-            phase_2 = State::LOW;
-            edge_2_callback(Edge{Edge::Front::FALLING, next_activation_time});
+            phase_2.set(State::LOW, next_activation_time, this);
             next_activation_time = next_phase_1;
             break;
     }
@@ -42,15 +39,15 @@ void DoubleClock::step()
     phase_count = (phase_count + 1) % 4;
 }
 
-State DoubleClock::get_phase_1_state() const { return phase_1; }
+State DoubleClock::get_phase_1_state() const { return *phase_1; }
 
-State DoubleClock::get_phase_2_state() const { return phase_2; }
+State DoubleClock::get_phase_2_state() const { return *phase_2; }
 
-void DoubleClock::register_phase_1_trigger(std::function<void(Edge)> callback)
+void DoubleClock::register_phase_1_trigger(const std::function<void(Edge)>& callback)
 {
-    edge_1_callback = std::move(callback);
+    phase_1.subscribe(callback);
 }
-void DoubleClock::register_phase_2_trigger(std::function<void(Edge)> callback)
+void DoubleClock::register_phase_2_trigger(const std::function<void(Edge)>& callback)
 {
-    edge_2_callback = std::move(callback);
+    phase_2.subscribe(callback);
 }
--- src/devices/src/DoubleClock.h
+++ src/devices/src/DoubleClock.h
@@ -5,7 +5,7 @@
 #include <emulation_core/src/Frequency.h>
 #include <emulation_core/src/Schedulable.h>
 #include <emulation_core/src/State.h>
-
+#include <emulation_core/src/OwnedSignal.h>
 #include <functional>
 
 class DoubleClock : public SchedulableImpl
@@ -17,19 +17,15 @@ public:
     [[nodiscard]] State get_phase_1_state() const;
     [[nodiscard]] State get_phase_2_state() const;
 
-    void register_phase_1_trigger(std::function<void(Edge)> callback);
-    void register_phase_2_trigger(std::function<void(Edge)> callback);
+    void register_phase_1_trigger(const std::function<void(Edge)>& callback);
+    void register_phase_2_trigger(const std::function<void(Edge)>& callback);
+
+    OwnedSignal phase_1;
+    OwnedSignal phase_2;
 
 private:
     Frequency main_frequency;
     Scheduling::counter_type next_phase_1 = 0;
-
-    State phase_1 = State::LOW;
-    State phase_2 = State::LOW;
-
-    std::function<void(Edge)> edge_1_callback = [](Edge) {};
-    std::function<void(Edge)> edge_2_callback = [](Edge) {};
-
     uint8_t phase_count = 0;
 };
 
