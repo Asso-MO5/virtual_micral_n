d3632fe290a51e265edd1d33282320346767c0d0
Sylvain Glaize <mokona@puupuu.org>
2021-06-21 22:28:52 +0200

    Remove "Update while running" option from the GUI.

--- src/gui/src/panels/Panel8008.cpp
+++ src/gui/src/panels/Panel8008.cpp
@@ -9,8 +9,6 @@ const char* state_to_name(uint state) { return STATE_NAMES[state]; }
 
 void display_8008_panel(const Simulator& simulator, uint64_t average_frequency)
 {
-    static bool running_update = true;
-
     const auto& scheduler = simulator.get_scheduler();
     const auto& clock_pulse = simulator.get_processor_card().get_debug_data().clock_pulse;
     const auto& cpu = simulator.get_processor_card().get_cpu();
@@ -22,73 +20,68 @@ void display_8008_panel(const Simulator& simulator, uint64_t average_frequency)
                 scheduler.get_counter() > 0 ? 1'000'000 * clock_pulse / scheduler.get_counter() : 0,
                 average_frequency);
 
-    ImGui::Checkbox("Update while running", &running_update);
+    auto cpu_debug_data = cpu.get_debug_data();
 
-    if (running_update)
+    ImGui::Text("Data Pins: %02x ",
+                simulator.get_processor_card().get_cpu().data_pins.get_value()); // Ouch...
+
     {
-        auto cpu_debug_data = cpu.get_debug_data();
+        ImGui::BeginChild("ChildLeft-Internal",
+                          ImVec2(ImGui::GetWindowContentRegionWidth() * 0.5f, 160), true);
 
-        ImGui::Text("Data Pins: %02x ",
-                    simulator.get_processor_card().get_cpu().data_pins.get_value()); // Ouch...
+        auto state = static_cast<uint>(*cpu.output_pins.state);
+        ImGui::Text("State %1d%1d%1d (%s)", (state >> 2) & 1, (state >> 1) & 1, (state >> 0) & 1,
+                    state_to_name(state));
 
+        auto cycle_control = static_cast<uint>(cpu.get_debug_data().cycle_control);
+        ImGui::Text("Cycle Control: %s",
+                    CYCLE_CONTROL_NAMES[static_cast<size_t>(cycle_control >> 6)]);
+
+        ImGui::Text("REG.a: %02x", cpu_debug_data.hidden_registers.a);
+        ImGui::Text("REG.b: %02x", cpu_debug_data.hidden_registers.b);
+
+        for (auto flag_index = 0; flag_index < IM_ARRAYSIZE(FLAG_NAMES); flag_index += 1)
         {
-            ImGui::BeginChild("ChildLeft-Internal",
-                              ImVec2(ImGui::GetWindowContentRegionWidth() * 0.5f, 160), true);
-
-            auto state = static_cast<uint>(*cpu.output_pins.state);
-            ImGui::Text("State %1d%1d%1d (%s)", (state >> 2) & 1, (state >> 1) & 1,
-                        (state >> 0) & 1, state_to_name(state));
-
-            auto cycle_control = static_cast<uint>(cpu.get_debug_data().cycle_control);
-            ImGui::Text("Cycle Control: %s",
-                        CYCLE_CONTROL_NAMES[static_cast<size_t>(cycle_control >> 6)]);
-
-            ImGui::Text("REG.a: %02x", cpu_debug_data.hidden_registers.a);
-            ImGui::Text("REG.b: %02x", cpu_debug_data.hidden_registers.b);
-
-            for (auto flag_index = 0; flag_index < IM_ARRAYSIZE(FLAG_NAMES); flag_index += 1)
-            {
-                ImGui::Text("%s: %s", FLAG_NAMES[flag_index],
-                            cpu_debug_data.flags[flag_index] ? "X" : "_");
-            }
-
-            ImGui::Text("%s @ %04x",
-                        cpu_debug_data.instruction_register == 0
-                                ? "---"
-                                : instruction_to_string(cpu_debug_data.decoded_instruction).c_str(),
-                        cpu_debug_data.latest_emitted_pci);
-
-            ImGui::EndChild();
+            ImGui::Text("%s: %s", FLAG_NAMES[flag_index],
+                        cpu_debug_data.flags[flag_index] ? "X" : "_");
         }
 
-        ImGui::SameLine();
+        ImGui::Text("%s @ %04x",
+                    cpu_debug_data.instruction_register == 0
+                            ? "---"
+                            : instruction_to_string(cpu_debug_data.decoded_instruction).c_str(),
+                    cpu_debug_data.latest_emitted_pci);
+
+        ImGui::EndChild();
+    }
+
+    ImGui::SameLine();
+    {
+        ImGui::BeginChild("ChildRight", ImVec2(0.f, 160), true);
+
+        ImGui::Text("Registers");
+
+        for (uint8_t r = 0; r < CPU8008::SCRATCH_PAD_SIZE; r++)
         {
-            ImGui::BeginChild("ChildRight", ImVec2(0.f, 160), true);
+            ImGui::Text("%s: %02x", REGISTER_NAMES[r], cpu_debug_data.registers[r]);
+        }
+        ImGui::EndChild();
+    }
 
-            ImGui::Text("Registers");
+    {
+        ImGui::BeginChild("ChildLeft-Stack",
+                          ImVec2(ImGui::GetWindowContentRegionWidth() * 0.5f, 180), true);
 
-            for (uint8_t r = 0; r < CPU8008::SCRATCH_PAD_SIZE; r++)
-            {
-                ImGui::Text("%s: %02x", REGISTER_NAMES[r], cpu_debug_data.registers[r]);
-            }
-            ImGui::EndChild();
+        ImGui::Text("Address Stack");
+        size_t stack_index = 0;
+        for (auto& address : cpu_debug_data.address_stack.stack)
+        {
+            ImGui::Text("%04x %s", cpu_debug_data.address_stack.stack[stack_index],
+                        stack_index == cpu_debug_data.address_stack.stack_index ? "<-" : "");
+            stack_index += 1;
         }
 
-        {
-            ImGui::BeginChild("ChildLeft-Stack",
-                              ImVec2(ImGui::GetWindowContentRegionWidth() * 0.5f, 180), true);
-
-            ImGui::Text("Address Stack");
-            size_t stack_index = 0;
-            for (auto& address : cpu_debug_data.address_stack.stack)
-            {
-                ImGui::Text("%04x %s", cpu_debug_data.address_stack.stack[stack_index],
-                            stack_index == cpu_debug_data.address_stack.stack_index ? "<-" : "");
-                stack_index += 1;
-            }
-
-            ImGui::EndChild();
-        }
+        ImGui::EndChild();
     }
 
     ImGui::End();
