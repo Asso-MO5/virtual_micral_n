fa534a338ec856251ad4b8d15b59e999d3f3c0b4
Sylvain Glaize <mokona@puupuu.org>
2021-07-14 21:31:57 +0200

    Add the possibility to connect OwnedValues of different underlying types.

--- src/emulation_core/src/ValueConnect.h
+++ src/emulation_core/src/ValueConnect.h
@@ -3,6 +3,38 @@
 
 #include "OwnedValue.h"
 
+template<typename ValueType, typename DestValueType>
+class MaskedValueConnector
+{
+public:
+    explicit MaskedValueConnector(OwnedValue<ValueType>& source_value, uint64_t mask,
+                                  int8_t right_shift, void* owner)
+        : source_value{source_value}, mask{mask}, right_shift{right_shift}, owner_id{owner}
+    {}
+
+    MaskedValueConnector<ValueType, DestValueType> to(OwnedValue<DestValueType>& destination_value)
+    {
+        const auto local_owner_id = owner_id;
+        const auto local_mask = mask;
+        const auto local_shift = right_shift;
+        destination_value.request(local_owner_id, Scheduling::counter_type{0});
+        source_value.subscribe(
+                [&destination_value, local_owner_id, local_mask,
+                 local_shift](ValueType, ValueType new_value, Scheduling::counter_type time) {
+                    const DestValueType masked_value = (new_value & local_mask) >> local_shift;
+                    destination_value.set(masked_value, time, local_owner_id);
+                });
+
+        return *this;
+    }
+
+private:
+    OwnedValue<ValueType>& source_value;
+    uint64_t mask;
+    int8_t right_shift;
+    void* owner_id;
+};
+
 template<typename ValueType>
 class ValueConnector
 {
@@ -13,15 +45,21 @@ public:
 
     ValueConnector<ValueType> to(OwnedValue<ValueType>& destination_value)
     {
-
         const auto local_owner_id = owner_id;
         destination_value.request(local_owner_id, Scheduling::counter_type{0});
-        source_value.subscribe([&destination_value, local_owner_id](uint8_t, uint8_t new_value,
+        source_value.subscribe([&destination_value, local_owner_id](ValueType, ValueType new_value,
                                                                     Scheduling::counter_type time) {
             destination_value.set(new_value, time, local_owner_id);
         });
 
         return *this;
+    }
+
+    template<typename DestValueType>
+    MaskedValueConnector<ValueType, DestValueType> mask(uint64_t mask, int8_t right_shift)
+    {
+        return MaskedValueConnector<ValueType, DestValueType>{source_value, mask, right_shift,
+                                                              owner_id};
     }
 
 private:
--- src/emulation_core/tests/value_connect_test.cpp
+++ src/emulation_core/tests/value_connect_test.cpp
@@ -12,6 +12,7 @@ namespace ValueMocks
     {
     public:
         OwnedValue<uint8_t> source_value;
+        OwnedValue<uint16_t> source_value_16;
     };
 
     class MockDestination
@@ -21,11 +22,12 @@ namespace ValueMocks
     };
 
 }
+
 TEST(Connect, connects_a_source_and_destination_values)
 {
     using namespace ValueMocks;
-    MockDestination destination_object;
     MockSource source_object;
+    MockDestination destination_object;
 
     auto connector = connect(source_object.source_value, this);
     connector.to(destination_object.destination_value);
@@ -37,4 +39,42 @@ TEST(Connect, connects_a_source_and_destination_values)
     source_object.source_value.set(0xfe, Scheduling::counter_type{100}, &source_object);
 
     ASSERT_THAT(*destination_object.destination_value, Eq(0xfe));
+}
+
+TEST(Connect, connects_a_source_and_destination_values_with_a_low_mask)
+{
+    using namespace ValueMocks;
+    MockDestination destination_object;
+    MockSource source_object;
+
+    connect(source_object.source_value_16, this)
+            .mask<uint8_t>(0x00ff, 0)
+            .to(destination_object.destination_value);
+
+    ASSERT_THAT(*source_object.source_value_16, Eq(0));
+    ASSERT_THAT(*destination_object.destination_value, Eq(0));
+
+    source_object.source_value_16.request(&source_object, Scheduling::counter_type{0});
+    source_object.source_value_16.set(0xfe, Scheduling::counter_type{100}, &source_object);
+
+    ASSERT_THAT(*destination_object.destination_value, Eq(0xfe));
+}
+
+TEST(Connect, connects_a_source_and_destination_values_with_a_high_mask)
+{
+    using namespace ValueMocks;
+    MockDestination destination_object;
+    MockSource source_object;
+
+    connect(source_object.source_value_16, this)
+            .mask<uint8_t>(0xff00, 8)
+            .to(destination_object.destination_value);
+
+    ASSERT_THAT(*source_object.source_value_16, Eq(0));
+    ASSERT_THAT(*destination_object.destination_value, Eq(0));
+
+    source_object.source_value_16.request(&source_object, Scheduling::counter_type{0});
+    source_object.source_value_16.set(0xfffe, Scheduling::counter_type{100}, &source_object);
+
+    ASSERT_THAT(*destination_object.destination_value, Eq(0xff));
 }
