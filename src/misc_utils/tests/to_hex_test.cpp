d6bde7a4ca9180d6740004f15a0baf872ebda351
Sylvain Glaize <mokona@puupuu.org>
2021-07-22 17:57:48 +0200

    Add a test for to_hex() utility function with unsigned 8 bit ints.

--- src/misc_utils/tests/to_hex_test.cpp
+++ src/misc_utils/tests/to_hex_test.cpp
@@ -6,16 +6,26 @@
 
 using namespace testing;
 
-TEST(ToHex, converts_16_bit_integers_with_padding) {
+TEST(ToHex, converts_16_bit_integers_with_padding)
+{
     const std::uint16_t VALUE = 0x1234;
     auto converted = utils::to_hex<std::uint16_t, 4>(VALUE);
 
     ASSERT_THAT(converted, Eq("$1234"));
 }
 
-TEST(ToHex, converts_16_bit_integers_with_runtime_padding) {
+TEST(ToHex, converts_16_bit_integers_with_runtime_padding)
+{
     const std::uint16_t VALUE = 0x1234;
     auto converted = utils::to_hex<std::uint16_t>(VALUE, 4);
 
     ASSERT_THAT(converted, Eq("$1234"));
+}
+
+TEST(ToHex, converts_8_bit_unsigned_integers_with_runtime_padding)
+{
+    const auto VALUE = static_cast<std::uint8_t>(0xff);
+    auto converted = utils::to_hex<std::uint8_t>(VALUE, 2);
+
+    ASSERT_THAT(converted, Eq("$ff"));
 }
