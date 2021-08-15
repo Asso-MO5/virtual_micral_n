bdbf9973cf8ad8df6f38938ff61c0a3f82972c03
Sylvain Glaize <mokona@puupuu.org>
2021-08-15 23:25:31 +0200

    Upgrade the span compatibility file for better detection.

--- src/misc_utils/src/span_compat.h
+++ src/misc_utils/src/span_compat.h
@@ -1,9 +1,21 @@
 #ifndef MICRALN_SPAN_COMPAT_H
 #define MICRALN_SPAN_COMPAT_H
 
-#ifdef __cpp_lib_concepts
-// It should be __cpp_lib_span, but it's not defined on gcc 10.3.0 even if it should.
+#if defined(__has_include)
+#if __has_include(<span>)
+#define HAS_SPAN
+#endif
+#endif
+
+#if defined(HAS_SPAN)
 #include <span>
+
+#if not defined(__cpp_lib_span)
+#pragma message(                                                                                   \
+        "span header was found but does not define std::span. Needs new compatibility case.")
+#error
+#endif
+
 #else
 #define TCB_SPAN_NAMESPACE_NAME std
 #include "span.hpp"
